d754d5a6ddc76132556bc074d101cc899aab09fe
Patrick Bryne <p.bryne@sthree.com>
2013-07-30 08:49:03 +0100

    add instance checks

--- lib/DynamicReturnTypePluginTestEnvironment/StaticCallTest.php
+++ lib/DynamicReturnTypePluginTestEnvironment/StaticCallTest.php
@@ -14,24 +14,44 @@ class StaticCallTest extends PhockitoTestCase {
 
     public function test_directStaticMethodCall_string() {
         $domDocument = Phockito::mock( '\DomDocument' );
-        Phockito::verify( $domDocument )->saveXml();
+        Phockito::verify( $domDocument )
+                ->saveXml();
     }
 
 
     public function test_parentStaticMethodCall_string() {
-        $domDocument = PhockitoChild::mock( '\DomDocument'  );
-        PhockitoChild::verify( $domDocument )->saveXml();
+        $domDocument = PhockitoChild::mock( '\DomDocument' );
+        PhockitoChild::verify( $domDocument )
+                ->saveXml();
     }
 
 
     public function test_directStaticMethodCall_constant() {
         $testEntity = Phockito::mock( TestEntity::CLASS_NAME );
-        PhockitoChild::verify( $testEntity )->getA();
+        PhockitoChild::verify( $testEntity )
+                ->getA();
     }
 
 
     public function test_parentStaticMethodCall_constant() {
         $testEntity = PhockitoChild::mock( TestEntity::CLASS_NAME );
-        PhockitoChild::verify( $testEntity )->getA();
+        PhockitoChild::verify( $testEntity )
+                ->getA();
     }
+
+
+    public function test_directStaticMethodCall_instance() {
+        $testEntity = Phockito::mock( new TestEntity( '', '', '' ) );
+        PhockitoChild::verify( $testEntity )
+                ->getA();
+    }
+
+
+    public function test_parentStaticMethodCall_instance() {
+        $testEntity = PhockitoChild::mock( new TestEntity( '', '', '' )  );
+        PhockitoChild::verify( $testEntity )
+                ->getA();
+    }
+
+
 }
