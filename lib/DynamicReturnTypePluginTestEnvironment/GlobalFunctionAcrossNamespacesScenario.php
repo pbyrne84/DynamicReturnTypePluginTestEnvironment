590fcf1e16ea8ae03c3e6895a532db52c9f56af1
pbyrne84 <pbyrne84@gmail.com>
2013-12-03 18:43:56 +0000

    add some more edge cases to overriden method handling from objects passed back from global functions

--- lib/DynamicReturnTypePluginTestEnvironment/GlobalFunctionAcrossNamespacesScenario.php
+++ lib/DynamicReturnTypePluginTestEnvironment/GlobalFunctionAcrossNamespacesScenario.php
@@ -19,6 +19,16 @@ namespace BaseReality\Provider {
         }
     }
 
+    class Provider3 {
+
+        /**
+         * @param $foo
+         * @return string
+         */
+        function make($foo) {
+        }
+    }
+
 
 }
 
@@ -38,9 +48,6 @@ namespace {
         return new \BaseReality\Provider\Provider2();
     }
 
-
-
-
 }
 
 namespace DifferentNameSpace {
@@ -53,11 +60,46 @@ namespace DifferentNameSpace {
 
     $provider2 = getProvider2();
 
-    $implementingObj2 = $provider2->make( "Non object value" );
+    $implementingObj2 = $provider2->make( "" );
     $implementingObj2->count();
-
 
     $provider1 = new Provider();
     $DOMDocument = $provider1->make( \DomDocument::class );
+    $DOMDocument->childNodes;
+
+}
+
+
+namespace a{
+    /**
+     * @return \BaseReality\Provider\Provider
+     */
+    function getProvider(){
+
+    }
+
+    /**
+     * @return \BaseReality\Provider\Provider3
+     */
+    function getProvider3() {
+        return new \BaseReality\Provider\Provider3();
+    }
+
+}
+
+
+namespace a{
+    use BaseReality\Provider\Provider3;
+
+    $provider = getProvider();
+
+    $implementingObj1 = $provider->make( \DomDocument::class );
+    $implementingObj1->createElement( "a" );
+
+    $provider3 = getProvider3();
+    $make = $provider3->make( "" );
+
+    $provider3b = new Provider3();
+    $make1 = $provider3b->make( "" );
 
 }
