8c1ae5ad600325f76198a446a88ba79e61261500
pbyrne84 <pbyrne84@gmail.com>
2014-04-04 21:52:40 +0100

    improve english slightly

--- ExecutingScriptApi.js
+++ ExecutingScriptApi.js
@@ -2,7 +2,7 @@
  * Javascript file to allow completion of com.ptby.dynamicreturntypeplugin.scripting.api.ExecutingScriptApi (JSDoc)
  * http://usejsdoc.org/tags-type.html
  *
- * When an api instance is injected into the rhino script it will replace a null instance so at the top of script
+ * When an api instance is injected into the rhino script it will replace a null instance so at the top of the script
  * add the following to enable completion :
  *
  * /**
