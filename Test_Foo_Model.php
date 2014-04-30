ea5ac1ee2c1be8b07c6466b6f89df7c1a1e0acba
pbyrne84 <pbyrne84@gmail.com>
2014-04-30 20:52:21 +0100

    and fluent interface for testing

--- Test_Foo_Model.php
+++ Test_Foo_Model.php
@@ -1,10 +1,19 @@
 <?php
 
-class Test_Foo_Model {
+class Test_Foo_Model
+{
     const CLASS_NAME = __CLASS__;
 
 
-    public function getFoo() {
+    public function getFoo(){
 
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function getThis(){
+        return $this;
     }
 }
