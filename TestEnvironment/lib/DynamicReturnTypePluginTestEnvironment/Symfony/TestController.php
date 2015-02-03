66fe0bafedf5ded99840f8a0c686fcbe096dc8be
PByrne84 <pbyrne84@gmail.com>
2015-02-03 09:26:22 +0000

    add extra test cases

--- TestEnvironment/lib/DynamicReturnTypePluginTestEnvironment/Symfony/TestController.php
+++ TestEnvironment/lib/DynamicReturnTypePluginTestEnvironment/Symfony/TestController.php
@@ -6,15 +6,44 @@ class TestController extends Controller {
     const CLASS_NAME = __CLASS__;
 
 
-    /**
-     * @Route("/app/example", name="homepage")
-     */
-    public function indexAction() {
+    public function test_stringParameter_leadingSlash() {
         $serviceBroker = $this->get( 'service_broker' );
-        $testService   = $serviceBroker->getServiceWithoutMask( TestService::CLASS_NAME );
+        $testService   = $serviceBroker->getServiceWithoutMask( '\DynamicReturnTypePluginTestEnvironment\TestClasses\TestService' );
 
         $testService->run();
 
         return $this->render( 'default/index.html.twig' );
     }
+
+    public function test_stringParameter_classConstantAlias() {
+        $serviceBroker = $this->get( 'service_broker' );
+        $testService   = $serviceBroker->getServiceWithoutMask( \DynamicReturnTypePluginTestEnvironment\TestClasses\TestService::CLASS_NAME );
+
+        $testService->run();
+
+        return $this->render( 'default/index.html.twig' );
+    }
+
+
+    public function test_stringParameter_classConstant() {
+        $serviceBroker = $this->get( 'service_broker' );
+        $testService   = $serviceBroker->getServiceWithoutMask( \DynamicReturnTypePluginTestEnvironment\TestClasses\TestService::class );
+
+        $testService->run();
+
+        return $this->render( 'default/index.html.twig' );
+    }
+
+
+
+    public function test_stringParameter_noLeadingSlash() {
+        $serviceBroker = $this->get( 'service_broker' );
+        $testService   = $serviceBroker->getServiceWithoutMask( 'DynamicReturnTypePluginTestEnvironment\TestClasses\TestService' );
+
+        $testService->run();
+
+        return $this->render( 'default/index.html.twig' );
+    }
+
+
 }
