ee2e14e05b014a3055123690eab58da651290790
pbyrne84 <pbyrne84@gmail.com>
2015-06-23 10:15:04 +0100

    add local variable  and field constant array access cases

--- TestEnvironment/lib/DynamicReturnTypePluginTestEnvironment/ArrayAccessTest.php
+++ TestEnvironment/lib/DynamicReturnTypePluginTestEnvironment/ArrayAccessTest.php
@@ -8,10 +8,31 @@ use DynamicReturnTypePluginTestEnvironment\OverriddenReturnType\Phockito;
 class ArrayAccessTest {
     const CLASS_NAME = __CLASS__;
 
+    /** @var  Phockito */
+    private $phockito;
 
-    public function test_variableArrayAccess(  ) {
+
+    public function test_variableStringArrayAccess(  ) {
         $phockito = new Phockito();
         $domDocument = $phockito[ '\DomDocument' ];
+        $domDocument->saveXml();
+    }
+
+    public function test_variableClassReferenceAccess(  ) {
+        $phockito = new Phockito();
+        $domDocument = $phockito[ \DomDocument::class ];
+        $domDocument->saveXml();
+    }
+
+
+    public function test_fieldStringArrayAccess(  ) {
+        $domDocument = $this->phockito[ '\DomDocument' ];
+        $domDocument->saveXml();
+    }
+
+
+    public function test_fieldClassReferenceAccess(  ) {
+        $domDocument = $this->phockito[ \DomDocument::class ];
         $domDocument->saveXml();
     }
 }
