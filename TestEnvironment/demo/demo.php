e5ffc5dfd2847526e50f2e7a7e71b0377ebfb414
Martin Hujer <mhujer@gmail.com>
2016-06-22 09:43:50 +0200

    autocomplete doesn't work when the plugin processes the value twice

--- TestEnvironment/demo/demo.php
+++ TestEnvironment/demo/demo.php
@@ -27,6 +27,22 @@ class Processor
 		$out->boo(); // does not work unless I copy the DataProvider class into this file
 	}
 
+	/**
+	 * @param \FooX[] $data
+	 */
+	public function processTwice(array $data)
+	{
+		$filteredData = Arr::filterArray($data, function (FooX $fooValue) {
+			return true;
+		});
+		$filteredData[0]->boo(); // works fine
+
+		$oneValue = Arr::getValueByCallback($filteredData, function (FooX $fooValue) {
+			return true;
+		});
+
+		$oneValue->boo(); // autocompletion does not work
+	}
 }
 
 class Arr
@@ -34,5 +50,19 @@ class Arr
 	public static function getFirst(array $a)
 	{
 		return $a[0];
+	}
+
+	public static function filterArray(array $a, callable $callback)
+	{
+		return array_filter($a, $callback);
+	}
+
+	public static function getValueByCallback(array $a, callable $callback)
+	{
+		foreach ($a as $item) {
+			if ($callback($item) === true) {
+				return $item;
+			}
+		}
 	}
 }
